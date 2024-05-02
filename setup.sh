941c8ffe10db97e2aee2a3ddb070afe2daa1bcc3
shadielhajj <shadi.elhajj@gmail.com>
2024-05-02 21:36:14 +0100

    Tweaked setup.sh and tweaked celeba config.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 apt-get -y install nano unzip psmisc libsm6 libxrender1 libfontconfig1 libxtst6
 
 conda env create -f environment.yaml
-source activate ldm
+
+# https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
+eval "$(conda shell.bash hook)"
+conda activate ldm
 
 pip install tensorboard vastai gpustat
